d08be488669a480386c0f64d4c3e6ad52978016f
ElinaBarseghyan <31693058+ElinaBarseghyan@users.noreply.github.com>
2017-12-04 14:46:49 +0400

    Update ArrayUtils.java

--- utils/ArrayUtils.java
+++ utils/ArrayUtils.java
@@ -1,16 +1,16 @@
 package utils;
 public class ArrayUtils {
 	public static void reverse(int[] arr) {
-		 for (int i = 0; i <= (arr.length - 1)/2; i++) {
-		 	arr[i] = arr[i] + arr[arr.length - 1 - i];
-		 	arr[arr.length - 1 - i] = arr[i] - arr[arr.length - 1 - i];
-		 	arr[i] = arr[i] - arr[arr.length - 1 - i];
+		 for (int i = 0; i < (arr.length - 1)/2; i++) {
+		 	arr[i] = arr[i] + arr[arr.length - i - 1];
+		 	arr[arr.length - i - 1] = arr[i] - arr[arr.length - i - 1];
+		 	arr[i] = arr[i] - arr[arr.length - i - 1];
 		 }
 	}
 	public static String stringifyArray(int[] arr) {
-		String result = "";
+		String result = "" + arr[0];
 		for(int i = 0; i < arr.length; i++) {
-			result = result + arr[i];
+			result = result + "," + arr[i];
 		}
 		return result;
 	}
